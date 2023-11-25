f45ec1748abb2aa72638436a9d6388f79b49c9fa
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-26 01:32:33 +0600

    menu item added

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -3,26 +3,38 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import { Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 import slide1 from '../../../assets//home/slide1.jpg'
 import slide2 from '../../../assets//home/slide2.jpg'
 import slide3 from '../../../assets//home/slide3.jpg'
 import slide4 from '../../../assets//home/slide4.jpg'
 import slide5 from '../../../assets//home/slide5.jpg'
+import SectionTitle from '../../../components/SectionTitle';
+
 
 const Category = () => {
     return (
         
-            <Swiper
+           <section>
+            <SectionTitle
+            subHeading={"From 11.00am to 10.00pm"}
+            heading={"Order Online"}
+            ></SectionTitle>
+             <Swiper
         slidesPerView={4}
         spaceBetween={30}
         centeredSlides={true}
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
-        className="mySwiper"
+        
+        modules={[Autoplay,Pagination]}
+        
       >
         <SwiperSlide>
             <img src={slide1} alt=''></img>
@@ -46,6 +58,7 @@ const Category = () => {
         </SwiperSlide>
         
       </Swiper>
+           </section>
         
     );
 };
---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import SectionTitle from '../../../components/SectionTitle';
+import MenuItem from '../../Shared/MenuItem/MenuItem';
+
+const PopularMenu = () => {
+    const [menu,setMenu]=useState([]);
+    useEffect(()=>{
+        fetch('menu.json')
+        .then(res=>res.json())
+        .then(data=>{
+            const popularItems=data.filter(item=>item.category==='popular')
+            console.log(popularItems)
+            setMenu(popularItems)
+        })
+    },[])
+    return (
+        <div className='mb-12'>
+             <SectionTitle
+            subHeading='From Our Menu'
+            heading='Popular Items'
+            ></SectionTitle>
+            <div className='grid md:grid-cols-2 gap-4'>
+                {
+                   menu.map(item=><MenuItem key={item._id} item={item}></MenuItem>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default PopularMenu;
