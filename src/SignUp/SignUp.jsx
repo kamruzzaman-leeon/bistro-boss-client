0062854ad673121d98e091fc013a63811d66f98e
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-12-04 15:40:44 +0600

    hook form

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,43 +1,52 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+    const { register, handleSubmit, formState: { errors }} = useForm();
+    const onSubmit = data => {
+
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Sign Up now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name</span>
-          </label>
-          <input type="text" placeholder="name" name='name' className="input input-bordered" required />
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign Up now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body" onSubmit={handleSubmit}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="name" {...register('name', { required: true })} className="input input-bordered" required />
+                            {errors.name?.type==='required' && <span className='text-red-600'>This Field is require</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" {...register('email', { required: true })} className="input input-bordered" required />
+                            {errors.email?.type==='required' && <span className='text-red-600'>This Field is require</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" {...register('password ', { required: true, minLength:6,maxLength:20 })} className="input input-bordered" required />
+                            {errors.password?.type==='required' && <span className='text-red-600'>This Field is require</span>}
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                        <p className='text-center'><small>Already Signed up Here?</small><small className='text-blue-600'> <Link to="/login">Login</Link></small></p>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" name='email' className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
-          
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Sign Up</button>
-        </div>
-        <p className='text-center'><small>Already Signed up Here?</small><small className='text-blue-600'> <Link to="/login">Login</Link></small></p>
-      </form>
-    </div>
-  </div>
-</div>
     );
 };
 
