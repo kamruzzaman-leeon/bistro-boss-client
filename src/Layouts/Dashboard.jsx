6465b2d30d7fc01cf4767f320be302bc69eed94c
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-12-22 21:36:54 +0600

    cart item deleted from database

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,8 +1,10 @@
 import { FaAd, FaBars, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 
 const Dashboard = () => {
+    const [cart]=useCart();
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-orange-400">
@@ -18,7 +20,7 @@ const Dashboard = () => {
 
                     <li><NavLink to="/dashboard/cart">
                         <FaShoppingCart></FaShoppingCart>
-                        My Cart</NavLink>
+                        My Cart ({cart.length})</NavLink>
                     </li>
                     <li><NavLink to="/dashboard/review">
                         <FaAd></FaAd>
@@ -36,14 +38,14 @@ const Dashboard = () => {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to='/order/pizza'>
+                        <NavLink to='/menu'>
                             <FaBars></FaBars>
                             Menu
                         </NavLink>
                     </li>
                 </ul>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
